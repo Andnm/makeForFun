0be07944085cbf134066acc2fbdcdac7e2f89d0d
Andnm <dangnguyenminhan123@gmail.com>
2022-03-08 11:34:06 +0700

    finish

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Bạn đã sẵn sàng chưa??',
+    introDesc: `Món quà bất ngờ đang chờ bạn đó`,
+    btnIntro: 'Chạm vào đây nè',
+    title: 'Bạn đang yêu Dũng đúng hông???',
+    desc: 'Thừa nhận đi, chạy không thoát đâu',
+    btnYes: 'Đúng rồi ó',
+    btnNo: 'Nooooooo',
+    question:'Cho xin cái ly do nào',
+    btnReply: 'Chấp nhận đi ahihi',
+    reply: 'Tại Dũng đẹp trai dễ thương siêu cấp vip vũ trụ',
+    mess: 'Hihihi, mình biết mà',
+    messDesc: 'Nhanh nhanh ib mình đi nào',
+    btnAccept: 'Bấm vào đây lẹ lẹ',
+    messLink: 'https://www.facebook.com/profile.php?id=100046113379075', //link fb
+    bgLink: 'utils/bg.webp', //link ảnh nền
+    iputBgLink: 'utils/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://scontent.fsgn5-4.fna.fbcdn.net/v/t1.6435-9/111614732_190914242455687_4122327662791404888_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=174925&_nc_ohc=Y_KGkOAzKL8AX8BHtfa&_nc_ht=scontent.fsgn5-4.fna&oh=00_AT8ZMTuPhBJbgfIP1EiQdCr_LPFr5_VUx10HmMBBg3Z1fw&oe=624AB6BA', //link ảnh lookMe
+    soundLink: 'utils/sound.mp3', //link nhac
+    duckLink: 'utils/duck.mp3', //link aam thanh 
+    swishLink: 'utils/Swish1.mp3', //link swish
+}
